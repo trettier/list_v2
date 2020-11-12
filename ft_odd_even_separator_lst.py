df07358e2696a703a67789cece83552f2d8d9df8
Vasilyev Ivan <tronpack@mail.ru>
2020-11-12 14:44:16 +0300

    4

--- ft_odd_even_separator_lst.py
+++ ft_odd_even_separator_lst.py
@@ -1,9 +1,12 @@
 def ft_odd_even_separator_lst(a):
-    c = [[], []]
+    b = []
+    c = []
+    d = []
     for i in a:
-        if type(i) == int:
-            if i % 2 == 0:
-                c[0].append(i)
-            else:
-                c[1].append(i)
-    return c
+        if i % 2 == 0:
+            b.append(i)
+        else:
+            c.append(i)
+    d.append(b)
+    d.append(c)
+    return d
