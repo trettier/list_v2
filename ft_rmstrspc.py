661e8b9e30b9593ae6c8bdd8e9cfbca2c2671958
Vasilyev Ivan <tronpack@mail.ru>
2020-11-12 12:41:45 +0300

    n

--- ft_rmstrspc.py
+++ ft_rmstrspc.py
@@ -1,13 +1,19 @@
-def ft_rmstrspc(a):
-    b = []
-    c = ""
+def ft_rmstrspc(a, b=" ", c=''):
+    n = 0
     for i in a:
-        b.append(i)
-    print(b)
-    for j in b:
-        if j == " ":
-            b.remove(j)
-    print(b)
-    for o in b:
-        c += o
-    return c
+        n += 1
+    n1 = 0
+    for i in b:
+        n1 += 1
+    d = ""
+    for i in range(n):
+        d += a[i]
+        if b in d:
+            d1 = ""
+            n2 = 0
+            for j in d:
+                n2 += 1
+            for j in range(n2 - n1):
+                d1 += d[j]
+            d = d1 + c
+    return d
